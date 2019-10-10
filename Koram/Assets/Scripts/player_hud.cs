16ff297ad4133170ec69a45c29d97e4db40b7f40
Cole <Coleman386@yahoo.com>
2019-10-10 10:52:55 -0400

    Added hud with time limit
    -need to add health bar

---
+++ Koram/Assets/Scripts/player_hud.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//We don't have to have a time limit,
+//it can easily be changed to be a hidden way to calculate score by how fast you complete a level
+
+public class player_hud : MonoBehaviour
+{
+    public float TimeLimit = 120;
+    public GameObject TimeLeftUI;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //every second, count down and then update the UI.
+        TimeLimit -= Time.deltaTime;
+        TimeLeftUI.gameObject.GetComponent<Text>().text = ("Time Left: " + (int)TimeLimit);
+
+        //if time reaches 0, reload level (restart level). temporary until we get a proper game over
+        if(TimeLimit < 0.1f) SceneManager.LoadScene("Koram");
+    }
+}
