fd0bf5d9b3258add0bbb0f476d7f4ac59841581f
justindeclark <justindeclark@gmail.com>
2019-10-17 14:53:53 -0400

    Jump mechanic Implimented

---
+++ Koram/Assets/Scripts/enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+
+    public float speed = 2;
+    Rigidbody2D body_enemy;
+    Transform transform_enemy;
+
+    public int damage = 5;//change to non static
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        body_enemy = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (gameObject.transform.position.x == 3) speed *= -1;//attempting to reverse direction
+        
+        //Always move forward
+        Vector2 myVel = body_enemy.velocity;
+        myVel.x = speed;
+        body_enemy.velocity = myVel;
+    }
+}
---
+++ Koram/Assets/Scripts/player_controller.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    //Movement
+    public int PlayerSpeed = 10;
+    public int PlayerJump = 10;
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+    public bool isGrounded = false;
+=======
+>>>>>>> 6cefe6eedb598d495873f3fe5b467cd86e4d2659
+=======
+>>>>>>> 008620c849675d8d26aeaa1a35bfcf0288b1173a
+    private float MoveX;
+    private bool FacingRight = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove(){
+        MoveX = Input.GetAxis("Horizontal");
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (MoveX * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+
+
+        //Flip Sprite
+        if (MoveX < 0.0f && FacingRight == true) FlipPlayer();
+        else if (MoveX > 0.0f && FacingRight == false) FlipPlayer();
+
+        //Jumping
+<<<<<<< HEAD
+<<<<<<< HEAD
+        if (Input.GetButtonDown("Jump") && isGrounded == true) GetComponent<Rigidbody2D>().velocity = new Vector2 (gameObject.GetComponent<Rigidbody2D>().velocity.x, PlayerJump);
+    }
+
+    
+
+=======
+        if (Input.GetButtonDown("Jump")) GetComponent<Rigidbody2D>().velocity = new Vector2 (gameObject.GetComponent<Rigidbody2D>().velocity.x, PlayerJump);
+    }
+
+>>>>>>> 6cefe6eedb598d495873f3fe5b467cd86e4d2659
+=======
+        if (Input.GetButtonDown("Jump")) GetComponent<Rigidbody2D>().velocity = new Vector2 (gameObject.GetComponent<Rigidbody2D>().velocity.x, PlayerJump);
+    }
+
+>>>>>>> 008620c849675d8d26aeaa1a35bfcf0288b1173a
+    void FlipPlayer()
+    {
+        Vector2 theScale = gameObject.transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        FacingRight = !FacingRight;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        GameObject myEnemy = GameObject.FindGameObjectWithTag("Enemy1");
+        enemy enemyScript = myEnemy.GetComponent<enemy>();
+        if (collision.collider.tag == "Enemy1"){
+            player_hud.PlayerHealth -= enemyScript.damage; 
+            Debug.Log(player_hud.PlayerHealth);
+        }
+    }
+
+}
