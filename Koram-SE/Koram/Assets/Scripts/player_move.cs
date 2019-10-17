b971c662b1b46480d377b9e668f12123991f04fa
andrewbaughman <52512098+andrewbaughman@users.noreply.github.com>
2019-10-17 19:54:47 -0400

    Revert "Merge branch 'master' of https://github.com/Trine-Software-Engineers/Koram-SE"
    This reverts commit 64650f43906acad71519d3c7a989804fdf0c8dd8, reversing
    changes made to a58e1a436521d23402c6af1a153ae6008c2d927e.

---
+++ Koram-SE/Koram/Assets/Scripts/player_move.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    public int PlayerSpeed = 10;
+    public int PlayerJump = 10;
+    private float MoveX;
+    private bool FacingRight = true;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //walking
+        MoveX = Input.GetAxis("Horizontal");
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (MoveX * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+
+        //Flip Sprite
+        if (MoveX < 0.0f && FacingRight == true) FlipPlayer();
+        else if (MoveX > 0.0f && FacingRight == false) FlipPlayer();
+
+        //Jumping
+        if (Input.GetButtonDown("Jump")) GetComponent<Rigidbody2D>().velocity = new Vector2 (gameObject.GetComponent<Rigidbody2D>().velocity.x, PlayerJump);
+
+    }
+
+    void FlipPlayer()
+    {
+        Vector2 theScale = gameObject.transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        FacingRight = !FacingRight;
+    }
+
+
+}
