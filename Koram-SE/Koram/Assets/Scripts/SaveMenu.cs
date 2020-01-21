ec0b0d971fa031a53eed4274bd4d9c60f247ec61
Coleman386 <Coleman386@yahoo.com>
2020-01-20 21:11:22 -0500

    Main Menu Level select overhaul, added new music

---
+++ Koram-SE/Koram/Assets/Scripts/SaveMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class SaveMenu : MonoBehaviour
+{
+    public static int LevelsCompleted = 0;
+    public TextMeshProUGUI LevelsCompletedUI;
+
+    void Awake()
+    {
+        
+    }
+
+    //This connects the volume in the settings menu to the actual volume of the sounds 
+    void Update()
+    {
+        SaveData SaveManager = GameObject.Find("SaveData").GetComponent<SaveData>();
+        LevelsCompletedUI.text = "Levels Completed: " + SaveManager.GetLevelsCompleted();
+    }
+
+    public void Save()
+    {
+        SaveData SaveManager = GameObject.Find("SaveData").GetComponent<SaveData>();
+        SaveManager.Save();
+    }
+
+    public void Load()
+    {
+        SaveData SaveManager = GameObject.Find("SaveData").GetComponent<SaveData>();
+        SaveManager.Load();
+    }
+
+    public void Delete()
+    {
+        SaveData SaveManager = GameObject.Find("SaveData").GetComponent<SaveData>();
+        SaveManager.Delete();
+    }
+}
