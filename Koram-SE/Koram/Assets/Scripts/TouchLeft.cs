fc352e90245858a302bcf2dc314bb2ccbc7643b2
Coleman386 <Coleman386@yahoo.com>
2020-01-29 17:16:11 -0500

    touch fix attempt 1

---
+++ Koram-SE/Koram/Assets/Scripts/TouchLeft.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TouchLeft : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+ 
+public static bool leftPressed;
+
+ 
+public void OnPointerDown(PointerEventData eventData){
+    if(this.gameObject.name == ("LeftButton")) leftPressed = true;
+}
+ 
+public void OnPointerUp(PointerEventData eventData){
+    if(this.gameObject.name == ("LeftButton")) leftPressed = false;
+}
+
+
+}
+
+
